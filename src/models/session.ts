8e4fe9a7977626e9e88bd93582a9c41cdb70b1fe
Shahar Duany <shahar.duany@gmail.com>
2022-06-29 22:55:05 +0300

    Added client template to src

--- src/models/session.ts
+++ src/models/session.ts
@@ -2,16 +2,17 @@ import sequelize from "./pg-sequelize";
 import store from "connect-session-sequelize";
 import buildSession from "express-session";
 
-export const session = buildSession({
-	secret: "mysecret",
-	resave: false,
-	saveUninitialized: false,
-});
-
 const SessionStore = store(buildSession.Store);
 
 const mySessionStore = new SessionStore({
 	db: sequelize
+});
+
+export const session = buildSession({
+	secret: "mysecret",
+	resave: false,
+	saveUninitialized: false,
+	store: mySessionStore
 });
 
 export default mySessionStore;
